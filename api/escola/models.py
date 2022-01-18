10b830b1cb3ff1988e8256c829528f5749bcbbf8
Mateus Marinho <mateus.marinho@hotmail.com.br>
2022-01-18 12:31:58 -0300

    simple api

---
+++ api/escola/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255)
+    rg = models.CharField(max_length=9)
+    
+    def __str__(self):
+        return self.nome
+
+    
